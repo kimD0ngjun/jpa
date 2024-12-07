26006532d8eda2c7cc3ed81df9284a563abc6ce8
kimD0ngjun <chickentasty0112@gmail.com>
2024-12-08 01:18:19 +0900

    feat: EntityGraph 기반 DAO 메소드 적용

--- src/main/java/com/example/jpa/nPlusOne/repository/ARepository.java
+++ src/main/java/com/example/jpa/nPlusOne/repository/ARepository.java
@@ -1,6 +1,7 @@
 package com.example.jpa.nPlusOne.repository;
 
 import com.example.jpa.nPlusOne.entity.A;
+import org.springframework.data.jpa.repository.EntityGraph;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
@@ -14,4 +15,7 @@ public interface ARepository extends JpaRepository<A,Long> {
             "LEFT JOIN FETCH a.bList b " +   // A와 B를 FetchJoin
             "LEFT JOIN FETCH a.cList c")     // A와 C를 FetchJoin
     List<A> findAllWithBAndC();
+
+    @EntityGraph(attributePaths = {"bList", "cList"})
+    List<A> findAll();
 }
