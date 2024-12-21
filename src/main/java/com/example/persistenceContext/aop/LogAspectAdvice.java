838438cbabfed1fa67f95768bb3a268c5a8f3a3d
kimD0ngjun <chickentasty0112@gmail.com>
2024-12-21 22:49:04 +0900

    refactor: 서비스 메소드별 AOP 적용

--- src/main/java/com/example/persistenceContext/aop/LogAspectAdvice.java
+++ src/main/java/com/example/persistenceContext/aop/LogAspectAdvice.java
@@ -10,6 +10,7 @@ import org.springframework.stereotype.Component;
  * AOP 어드바이스 : 특정 메소드 실행 전후에 삽입될 행동 정의
  * (여기서는 실행 전에 메소드 서명을 로깅하고 실행 후에는 실행 결과 로깅)
  * @Around 어노테이션 기반 구현, AspectJ 스타일
+ * 서비스의 find() 메소드 실행 전후의 AOP 세팅
  */
 @Slf4j
 @Aspect
@@ -20,10 +21,11 @@ public class LogAspectAdvice {
     @Around("execution(void com.example.persistenceContext.aop.AopService.find(..))")
     public Object execute(ProceedingJoinPoint joinPoint) throws Throwable {
         String message = joinPoint.getSignature().toShortString(); // 메소드 서명(이름, 매개변수 등) 갖고오기
+        log.info("실행 메소드를 찾기 위한 ProceedingJoinPoint 객체 메소드 기반 조회");
 
         Object result = joinPoint.proceed(); // 메소드 실행
 
-        log.info("메소드 실행 결과: {}", message); // 실행된 메소드의 결과 출력
+        log.info("실행 메소드 정체: {}", message); // 실행된 메소드의 결과 출력
 
         return result; // 메소드 실행 결과 반환
     }
--- src/main/java/com/example/persistenceContext/aop/TimeAdvice.java
+++ src/main/java/com/example/persistenceContext/aop/TimeAdvice.java
@@ -9,6 +9,7 @@ import org.springframework.stereotype.Component;
  * AOP 어드바이스 : 특정 메소드 실행 전후에 삽입될 행동 정의
  * (여기서는 실행 시간을 계측해서 로깅을 남김)
  * MethodInterceptor 인터셉터 구현 방식으로 실행 전후의 어드바이스 구현, 전통적인 스타일
+ * 서비스의 save() 메소드 실행 전후의 AOP 세팅
  */
 @Slf4j
 @Component
@@ -16,12 +17,12 @@ public class TimeAdvice implements MethodInterceptor {
     @Override
     public Object invoke(MethodInvocation invocation) throws Throwable {
         log.info("TimeAdvice.invoke() 실행");
-        long startMs = System.currentTimeMillis(); // 메소드 실행 전, 실행시간 기록
+        long start = System.nanoTime(); // 메소드 실행 전, 실행시간 기록
 
         Object target = invocation.proceed(); // 메소드 실행
 
-        long endMs = System.currentTimeMillis();
-        log.info("TimeAdvice.invoke() 종료, 실행 시간 {} ms", endMs - startMs); // 메소드 실행 후, 실행시간 기록
+        long end = System.nanoTime();
+        log.info("TimeAdvice.invoke() 종료, 실행 시간 {} ns", end - start); // 메소드 실행 후, 실행시간 기록
 
         return target; // 메소드 실행 결과 반환
     }
