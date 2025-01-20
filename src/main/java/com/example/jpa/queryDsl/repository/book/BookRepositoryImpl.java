23c92150700bc2f17026d68d4ce87f31c4f6e5ca
kimD0ngjun <chickentasty0112@gmail.com>
2025-01-20 18:00:55 +0900

    feat: 간단한 조건 기반 조회

--- src/main/java/com/example/jpa/queryDsl/repository/book/BookRepositoryImpl.java
+++ src/main/java/com/example/jpa/queryDsl/repository/book/BookRepositoryImpl.java
@@ -1,5 +1,7 @@
 package com.example.jpa.queryDsl.repository.book;
 
+import com.example.jpa.queryDsl.entity.Book;
+import com.example.jpa.queryDsl.entity.QBook;
 import com.querydsl.jpa.impl.JPAQueryFactory;
 import lombok.RequiredArgsConstructor;
 
@@ -8,4 +10,11 @@ public class BookRepositoryImpl implements CustomBookRepository {
 
     private final JPAQueryFactory queryFactory;
 
+    // 간단한 조건
+    @Override
+    public Book findBookByTitle(String title) {
+        QBook book = QBook.book;
+
+        return queryFactory.selectFrom(book).where(book.title.eq(title)).fetchOne();
+    }
 }
--- src/main/java/com/example/jpa/queryDsl/repository/book/CustomBookRepository.java
+++ src/main/java/com/example/jpa/queryDsl/repository/book/CustomBookRepository.java
@@ -1,4 +1,9 @@
 package com.example.jpa.queryDsl.repository.book;
 
+import com.example.jpa.queryDsl.entity.Book;
+
 public interface CustomBookRepository {
+
+    // 조건(Where)
+    Book findBookByTitle(String title);
 }
