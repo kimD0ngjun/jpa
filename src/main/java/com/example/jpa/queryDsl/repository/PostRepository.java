5c26dc50ff8863def4cb6dfeb690afcdecc94bb0
kimD0ngjun <chickentasty0112@gmail.com>
2025-01-20 17:02:05 +0900

    fix: QUeryDSL 테스트용 엔티티 재생성을 위한 삭제 처리

--- src/main/java/com/example/jpa/queryDsl/repository/PostRepository.java
+++ src/main/java/com/example/jpa/queryDsl/repository/PostRepository.java
@@ -1,12 +1,11 @@
 package com.example.jpa.queryDsl.repository;
 
-import com.example.jpa.queryDsl.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 /**
  * JpaRepository 인터페이스 (기본 CRUD 제공)
  */
-@Repository
-public interface PostRepository extends JpaRepository<Post, Long>, CustomPostRepository {
-}
+//@Repository
+//public interface PostRepository extends JpaRepository<Post, Long>, CustomPostRepository {
+//}
