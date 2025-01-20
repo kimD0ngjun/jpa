dc11065f82bec8719e563506b4175d5df8a8acb9
kimD0ngjun <chickentasty0112@gmail.com>
2025-01-20 17:14:31 +0900

    feat: 연관관계를 위한 엔티티 재설정

---
+++ src/main/java/com/example/jpa/queryDsl/entity/Author.java
@@ -0,0 +1,29 @@
+package com.example.jpa.queryDsl.entity;
+
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity
+@Table(name = "author")
+@Getter
+@Setter
+public class Author {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+
+    @OneToMany(mappedBy = "author", cascade = CascadeType.ALL)
+    private List<Book> book = new ArrayList<>();
+
+    @ManyToOne(fetch=FetchType.LAZY)
+    @JoinColumn(name = "organization_id")
+    private Organization organization;
+
+}
