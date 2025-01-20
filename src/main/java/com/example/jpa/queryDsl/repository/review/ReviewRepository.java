9440aba858cc20ade188532597a2a35943c5f758
kimD0ngjun <chickentasty0112@gmail.com>
2025-01-20 17:38:33 +0900

    feat: 리뷰 관련 사용자 정의 레포 세팅

---
+++ src/main/java/com/example/jpa/queryDsl/repository/review/ReviewRepository.java
@@ -0,0 +1,9 @@
+package com.example.jpa.queryDsl.repository.review;
+
+import com.example.jpa.queryDsl.entity.Review;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface ReviewRepository extends JpaRepository<Review, Long>, CustomReviewRepository {
+}
