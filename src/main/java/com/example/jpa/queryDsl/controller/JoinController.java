acc1a2a6d17dccfc0415f21d301f023c863dc471
kimD0ngjun <chickentasty0112@gmail.com>
2025-01-21 16:57:19 +0900

    feat: 자기 자신과의 참조에 대한 별칭 적용

--- src/main/java/com/example/jpa/queryDsl/controller/JoinController.java
+++ src/main/java/com/example/jpa/queryDsl/controller/JoinController.java
@@ -3,6 +3,7 @@ package com.example.jpa.queryDsl.controller;
 import com.example.jpa.queryDsl.entity.Book;
 import com.example.jpa.queryDsl.repository.author.AuthorRepository;
 import com.example.jpa.queryDsl.repository.book.BookRepository;
+import com.example.jpa.queryDsl.repository.self.SelfRepository;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,6 +14,7 @@ public class JoinController {
 
     private final BookRepository bookRepository;
     private final AuthorRepository authorRepository;
+    private final SelfRepository selfRepository;
 
     @GetMapping("/join1")
     public void test1() {
@@ -35,6 +37,14 @@ public class JoinController {
         System.out.println(bookRepository.fetchJoin()
                 .stream()
                 .map(e -> "제목 : " + e.getTitle() + ", 작가 : " + e.getAuthor().getName() + "\n")
+                .toList());
+    }
+
+    @GetMapping("/join4")
+    public void test4() {
+        System.out.println(selfRepository.selfJoin()
+                .stream()
+                .map(e -> "이름 : " + e.getName() + "\n")
                 .toList());
     }
 }
