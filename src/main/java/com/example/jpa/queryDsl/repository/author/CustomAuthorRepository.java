a5077a2b7b803c9f1908603648e7b219f4928125
kimD0ngjun <chickentasty0112@gmail.com>
2025-01-23 20:51:10 +0900

    feat: from + 서브쿼리는 QueryDSL에서 불가능

--- src/main/java/com/example/jpa/queryDsl/repository/author/CustomAuthorRepository.java
+++ src/main/java/com/example/jpa/queryDsl/repository/author/CustomAuthorRepository.java
@@ -22,4 +22,10 @@ public interface CustomAuthorRepository {
 
     // Select * 서브쿼리
     List<Tuple> selectSubquery();
+
+    /**
+     * From + 서브쿼리는 불가능하다
+     * JPA 표준에서 하위 쿼리의 FROM 절에서는 엔터티나 엔터티 경로만 사용할 수 있으며 서브쿼리를 지원하지 않는다.
+     * QueryDSL은 태생적으로 JPA에서 발전된 기술이기 때문에 기존의 이러한 제약을 따른다.
+     */
 }
